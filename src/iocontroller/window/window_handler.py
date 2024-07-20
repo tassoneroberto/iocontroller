634a57a2ba7580eb14557bf55a4fe79a90d99989
Roberto Tassone <roberto.tassone@proton.me>
2024-07-20 16:21:16 +0200

    feat: wait for the window to be active before returning
    Signed-off-by: Roberto Tassone <roberto.tassone@proton.me>

--- src/iocontroller/window/window_handler.py
+++ src/iocontroller/window/window_handler.py
@@ -9,30 +9,38 @@ logging.getLogger(__name__)
 logging.root.setLevel(logging.INFO)
 
 
-def select_window(window_name, delay = .5):
-    logging.info(f"Detecting {window_name}...")
+def select_window(window_name: str) -> tuple[int, int, int, int]:
+    logging.info(f"Checking for application window with title [{window_name}]...")
     hwnd = win32gui.FindWindow(None, r"{}".format(window_name))
     if hwnd == 0:
-        raise Exception(f"Application {window_name} not detected.")
-    win32gui.SetForegroundWindow(hwnd)
+        raise Exception(f"Application [{window_name}] not detected.")
+
     dimensions = win32gui.GetWindowRect(hwnd)
     width = dimensions[2] - dimensions[0]
     height = dimensions[3] - dimensions[1]
     logging.info(
-        f"Application {window_name} detected successfully. Window size: [{width}x{height}]"
+        f"Application [{window_name}] detected successfully. Window size: [{width}x{height}]"
     )
-    time.sleep(delay) # Make sure the application is on focus before returning
+
+    win32gui.SetForegroundWindow(hwnd)
+
+    while window_name != win32gui.GetWindowText(win32gui.GetForegroundWindow()):        
+        logging.info(f"Setting it as active window...")
+        time.sleep(1)
+
+    logging.info(f"The selected window is now active.")
+
     return dimensions
 
 
-def get_relative_window_center(dimensions):
+def get_relative_window_center(dimensions: tuple[int, int, int, int]) -> list[int]:
     return [
         (dimensions[2] - dimensions[0]) // 2,
         (dimensions[3] - dimensions[1]) // 2,
     ]
 
 
-def get_absolute_window_center(dimensions):
+def get_absolute_window_center(dimensions: tuple[int, int, int, int]) -> list[int]:
     return [
         (dimensions[2] - dimensions[0]) // 2 + dimensions[0],
         (dimensions[3] - dimensions[1]) // 2 + dimensions[1] + 20,
